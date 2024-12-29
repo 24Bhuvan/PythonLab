39ec0bb374b123409e3e5345d219e2284b7bf159
24Bhuvan <ubhuvan533@gmail.com>
2024-12-29 21:40:36 +0530

    new file

---
+++ Hcf.py
@@ -0,0 +1,15 @@
+def Hcf():
+    x=int(input("Enter your number1:"))
+    y=int(input("Enter your number1:"))
+    if x>y:
+        num=x
+    else:
+        num=y
+    while 1:
+        if ((num%x==0)and(num%y==0)):
+            lcm=num
+            break
+        num-=1
+    print(f'{num} is the Highest common factor')
+if __name__ == "__main__":
+    Hcf()
